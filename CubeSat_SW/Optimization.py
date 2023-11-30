ff9c4f098604a7882a8e948b92f3676bc4c9874f
Cristea Teodor - Andrei <109206006+CristeaTeodorAndrei@users.noreply.github.com>
2023-11-30 17:46:37 +0200

    feat: Minor changes
    Created a special test case to verify if the service has been disabled or not.

--- CubeSat_SW/Optimization.py
+++ CubeSat_SW/Optimization.py
@@ -1,18 +1,62 @@
 import subprocess
 
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    RESET = '\033[0m'
+    
+def install_smbus2():
+    try:
+        
+    subprocess.check_call('')
+    
 def disable_wifi():
-    subprocess.run(["sudo", "ifconfig", "wlan0", "down"])
-
+    try:
+        subprocess.check_output(['sudo', 'ifconfig', 'wlan0', 'down'])
+        print("Wi-Fi is already deactivated!")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.check_call(['sudo', 'ifconfig', 'wlan0', 'down'])
+            print("Deactivating Wi-Fi module!")
+        except subprocess.CalledProcessError as e:
+            print(f"Error trying to disable Wi-Fi module: {e}")
+    
 def disable_gui():
-    subprocess.run(["sudo", "systemctl", "set-default", "multi-user.target"])
-    subprocess.run(["sudo", "systemctl", "disable", "lightdm.service"])
+    try:
+        subprocess.check_output(['sudo', 'systemctl', 'is-active', '--quiet', 'lightdm'])
+        print("GUI is already disabled!")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.check_call(['sudo', 'systemctl', 'stop', 'lightdm'])
+            print("GUI has been disabled!")
+        except subprocess.CalledProcessError as e:
+            print(f"Error trying to disable GUI: {e}")
 
+    
 def disable_bluetooth():
-    subprocess.run(["sudo", "systemctl", "disable", "bluetooth.service"])
-    subprocess.run(["sudo", "systemctl", "disable", "hciuart.service"])
+    try:
+        subprocess.check_output(['sudo', 'systemctl', 'is-enabled', '--quiet', 'bluetooth.service'])
+        print("Bluetooth is already disabled!")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.check_call(['sudo', 'systemctl', 'disable', 'bluetooth.service'])
+            print("Deactivating Bluetooth module!")
+        except subprocess.CalledProcessError as e:
+            print(f"Error trying to deactivate Bluetooth module: {e}")
 
+    try:
+        subprocess.check_output(['sudo', 'systemctl', 'is-enabled', '--quiet', 'hciuart.service'])
+        print("HCIUART already deactivated!")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.check_call(['sudo', 'systemctl', 'disable', 'hciuart.service'])
+            print("Deactivating HCIUART service!")
+        except subprocess.CalledProcessError as e:
+            print(f"Error trying to deactivate HCIUART service: {e}")
+            
 def disable_updates():
     subprocess.run(["sudo", "systemctl", "disable", "apt-daily.timer"])
     subprocess.run(["sudo", "systemctl", "disable", "apt-daily-upgrade.timer"])
     subprocess.run(["sudo", "systemctl", "stop", "apt-daily.service"])
     subprocess.run(["sudo", "systemctl", "stop", "apt-daily-upgrade.service"])
+    print("Updates have been disabled!")
