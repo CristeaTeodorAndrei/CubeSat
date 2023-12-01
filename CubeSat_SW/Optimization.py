d0e95a9185600a20038207d0ddb0ef9fadc2589c
GMW <cristeaandrei959@yahoo.com>
2023-12-01 15:49:53 +0200

    fix: Trying to fix GUI error (#000)

--- CubeSat_SW/Optimization.py
+++ CubeSat_SW/Optimization.py
@@ -52,12 +52,13 @@ def disable_wifi():
     
 def disable_gui():
     try:
-        subprocess.check_call(['sudo', 'systemctl', 'stop', 'lightdm'], stderr=subprocess.DEVNULL)
         print(f"{Color_schema.Colors.GREEN}GUI Service - Deactivated{Color_schema.Colors.RESET}")
         print("\n\n")
         print(f"{Color_schema.Colors.GREEN}All system configurations have been updated!{Color_schema.Colors.RESET}")
         print(f"{Color_schema.Colors.RED}OnBoard Computer will restart in 10 seconds!{Color_schema.Colors.RESET}") 
         time.sleep(10)
+        subprocess.run(['sudo', 'systemctl', 'stop', 'lightdm'], stderr=subprocess.DEVNULL)
+        subprocess.run(['sudo', 'systemctl', 'isolate', 'multi-user.target'], check=True, stderr=subprocess.DEVNULL)
         subprocess.run(['sudo', 'reboot'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"{Color_schema.Colors.RED}Error trying to disable GUI service: {e.returncode} - {e.stderr.decode().strip()}{Color_schema.Colors.RESET}")
