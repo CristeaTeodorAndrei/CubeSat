c0de87c2a103b9ee12a713d9a287ba8632a18661
GMW <cristeaandrei959@yahoo.com>
2023-12-01 08:57:09 +0200

    fix: Fixing Updates function

--- CubeSat_SW/Optimization.py
+++ CubeSat_SW/Optimization.py
@@ -10,14 +10,14 @@ def dependencies():
         subprocess.run(['python3', '--version'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run(['pip3', '--version'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run(['sudo', 'apt-get', 'install', '-y', 'i2c-tools'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print(f"{Color_schema.Colors.GREEN}System dependencies - Installed!{Color_schema.Colors.RESET}")
+        print(f"{Color_schema.Colors.GREEN}System dependencies are already installed!{Color_schema.Colors.RESET}")
         print("\n")
 
     except subprocess.CalledProcessError:
         try:
             subprocess.run(['sudo', 'apt-get', 'update'], check=True, stderr=subprocess.DEVNULL)
             subprocess.run(['sudo', 'apt-get', 'install', '-y', 'python3', 'python3-pip', 'i2c-tools'], check=True, stderr=subprocess.DEVNULL)
-            print(f"{Color_schema.Colors.GREEN}System dependencies - Installed!{Color_schema.Colors.RESET}")
+            print(f"{Color_schema.Colors.GREEN}System dependencies installed successfully!{Color_schema.Colors.RESET}")
             print("\n")
         except subprocess.CalledProcessError as e:
             print(f"{Color_schema.Colors.RED}Error installing system dependencies: {e.returncode}{Color_schema.Colors.RESET}")
@@ -25,18 +25,27 @@ def dependencies():
             
 
     try:
-        subprocess.run(['sudo', 'raspi-config', 'nonint', 'do_i2c', '0'], check=True)
-        print(f"{Color_schema.Colors.GREEN}I2C activated successfully!{Color_schema.Colors.RESET}")
-    except subprocess.CalledProcessError as e:
-        print(f"{Color_schema.Colors.RED}Error activating I2C: {e.returncode}{Color_schema.Colors.RESET}")
+        subprocess.run(['sudo', 'raspi-config', 'nonint', 'do_i2c', '0'], check=True, stderr=subprocess.DEVNULL)
+        print(f"{Color_schema.Colors.GREEN}I2C is already activated!{Color_schema.Colors.RESET}")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.run(['sudo', 'raspi-config', 'nonint', 'do_i2c', '0'], check=True)
+            print(f"{Color_schema.Colors.GREEN}I2C activated successfully!{Color_schema.Colors.RESET}")
+        except subprocess.CalledProcessError as e:
+            print(f"{Color_schema.Colors.RED}Error activating I2C: {e.returncode}{Color_schema.Colors.RESET}")
 
     try:
         subprocess.run(['sudo', 'raspi-config', 'nonint', 'do_spi', '0'], check=True, stderr=subprocess.DEVNULL)
-        print(f"{Color_schema.Colors.GREEN}SPI activated successfully!{Color_schema.Colors.RESET}")
+        print(f"{Color_schema.Colors.GREEN}SPI is already activated!{Color_schema.Colors.RESET}")
         print("\n")
-    except subprocess.CalledProcessError as e:
-        print(f"{Color_schema.Colors.RED}Error activating SPI: {e.returncode}{Color_schema.Colors.RESET}")
-        print("\n")
+    except subprocess.CalledProcessError:
+        try:
+            subprocess.run(['sudo', 'raspi-config', 'nonint', 'do_spi', '0'], check=True, stderr=subprocess.DEVNULL)
+            print(f"{Color_schema.Colors.GREEN}SPI activated successfully!{Color_schema.Colors.RESET}")
+            print("\n")
+        except subprocess.CalledProcessError as e:
+            print(f"{Color_schema.Colors.RED}Error activating SPI: {e.returncode}{Color_schema.Colors.RESET}")
+            print("\n")
 
 
     for package in python_packages:
@@ -83,21 +92,13 @@ def disable_bluetooth():
 
 def disable_updates():
     try:
-        subprocess.run(["sudo", "systemctl", "is-active", "--quiet", "apt-daily.timer"], stderr=subprocess.DEVNULL)
-        print(f"{Color_schema.Colors.GREEN}Automatic updates service is already deactivated!{Color_schema.Colors.RESET}")
-    except subprocess.CalledProcessError:
-        print("Deactivating automatic updates service", end='', flush=True)
-        try:
-            subprocess.run(["sudo", "systemctl", "disable", "apt-daily.timer"], stderr=subprocess.DEVNULL)
-            print(".", end='', flush=True)
-            subprocess.run(["sudo", "systemctl", "disable", "apt-daily-upgrade.timer"], stderr=subprocess.DEVNULL)
-            print(".", end='', flush=True)
-            subprocess.run(["sudo", "systemctl", "stop", "apt-daily.service"], stderr=subprocess.DEVNULL)
-            print(".", end='', flush=True)
-            subprocess.run(["sudo", "systemctl", "stop", "apt-daily-upgrade.service"], stderr=subprocess.DEVNULL)
-            print(f"\n{Color_schema.Colors.ORANGE}Automatic updates have been successfully disabled!{Color_schema.Colors.RESET}")
-        except subprocess.CalledProcessError as e:
-            print(f"\n{Color_schema.Colors.RED}Error while disabling automatic updates: {e.returncode}{Color_schema.Colors.RESET}")
+        subprocess.run(["sudo", "systemctl", "disable", "apt-daily.timer"], stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "systemctl", "disable", "apt-daily-upgrade.timer"], stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "systemctl", "stop", "apt-daily.service"], stderr=subprocess.DEVNULL)
+        subprocess.run(["sudo", "systemctl", "stop", "apt-daily-upgrade.service"], stderr=subprocess.DEVNULL)
+        print(f"{Color_schema.Colors.ORANGE}Automatic updates have been successfully disabled!{Color_schema.Colors.RESET}")
+    except subprocess.CalledProcessError as e:
+        print(f"{Color_schema.Colors.RED}Error while disabling automatic updates: {e.returncode} - {e.stderr.decode().strip()}{Color_schema.Colors.RESET}")
 
 def optimization_start():
     dependencies()
@@ -105,3 +106,5 @@ def optimization_start():
     disable_updates()
     disable_bluetooth()
     disable_gui()
+
+    ## verifica daca disable updates crapa
