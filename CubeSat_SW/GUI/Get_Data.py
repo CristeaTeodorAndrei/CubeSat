262bb49c6773ba747f13391c52a032151c2dabc5
Cristea Teodor - Andrei <109206006+CristeaTeodorAndrei@users.noreply.github.com>
2024-05-23 22:40:43 +0300

    feat: Updated the Get_Data.py to store the array that comes from CubeSAt

--- CubeSat_SW/GUI/Get_Data.py
+++ CubeSat_SW/GUI/Get_Data.py
@@ -1,26 +1,42 @@
 import serial.tools.list_ports
 import threading
+import time
 
-# Caută portul la care este conectat Arduino-ul
+# Find the port to which the Arduino is connected
 def cautare_port():
-    ports = serial.tools.list_ports.comports()
-    for port, desc, hwid in sorted(ports):
-        if "Arduino" in desc:
-            return port
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if "CH340" in port.description:
+            return port.device
+    return None
 
-# Funcție pentru citirea datelor de la Arduino
-def citeste_date(port):
-    with serial.Serial(port, 9600, timeout=1) as ser:
+# Function to read data from Arduino
+def citeste_date(port, data_list):
+    with serial.Serial(port, 115200, timeout=1) as ser:
         while True:
             line = ser.readline().decode().strip()
-            print(line)
+            if line:
+                try:
+                    # Split the line into individual string numbers, then convert them to floats
+                    numbers = [float(x) for x in line.split()]
+                    data_list.append(numbers)
+                except ValueError as e:
+                    print(f"Error converting data to float: {e}")
+                    continue
 
 if __name__ == "__main__":
+    data_list = []
     port = cautare_port()
     if port:
-        thread = threading.Thread(target=citeste_date, args=(port,))
+        thread = threading.Thread(target=citeste_date, args=(port, data_list))
         thread.daemon = True
         thread.start()
-        thread.join()
-    else:
-        print("Nema.")
+
+    #     try:
+    #         while True:
+    #             print(data_list)
+    #             time.sleep(1)  # Print the array every second
+    #     except KeyboardInterrupt:
+    #         print("Stopped by user")
+    # else:
+    #     print("No port found.")
