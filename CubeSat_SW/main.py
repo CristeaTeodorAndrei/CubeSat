24854999dbf0f6df99d313184f6211b7dc983eea
Cristea Teodor - Andrei <109206006+CristeaTeodorAndrei@users.noreply.github.com>
2024-06-04 20:00:18 +0300

    fix: Updated main.py

--- CubeSat_SW/main.py
+++ CubeSat_SW/main.py
@@ -1,60 +1,66 @@
 import time
 import struct
-from RF24 import RF24, RF24_PA_LOW
-from Nominal import read_sensor_data
-# Initialization of the RF24 module
-CSN_PIN = 0  # GPIO8 aka CE0 on SPI bus 0: /dev/spidev0.0
+from RF24 import RF24, RF24_PA_HIGH
+from Sampling import *
+import RPi.GPIO as GPIO
+
+CSN_PIN = 0  
 CE_PIN = 22
 radio = RF24(CE_PIN, CSN_PIN)
+LED1_PIN = 20
+LED2_PIN = 21
+Charge = 16
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED1_PIN, GPIO.OUT)
+GPIO.setup(LED2_PIN, GPIO.OUT)
+GPIO.setup(Charge, GPIO.OUT)
 
 if not radio.begin():
     raise RuntimeError("radio hardware is not responding")
 
 address = [b"1Node", b"2Node"]
 radio_number = 1
-radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
-radio.payloadSize = 32  # Set to maximum payload size supported by nRF24L01+
+radio.setPALevel(RF24_PA_HIGH)
+radio.openWritingPipe(address[radio_number])
+radio.payloadSize = 32
 
-
-
-def transmit_data():
-    radio.stopListening()  # put radio in TX mode
+def main():
+    radio.stopListening()
     while True:
         vector = read_sensor_data()
-        
-        # Split the vector into two parts
-        first_part = struct.pack("<8f", *vector[:8])  # First 8 floats
-
-        # Start timer
+        print("Sensor Data:", vector)
+        BAT = vector[-1]
+        if BAT < 80:
+            GPIO.output(Charge, GPIO.HIGH)
+        else:
+            GPIO.output(Charge, GPIO.LOW)
+            
+        first_part = struct.pack("<8f", *vector[:8])
         start_timer = time.monotonic_ns()
-
-        # Transmit the first part
         result1 = radio.write(first_part)
-
-        # Measure end time
         end_timer = time.monotonic_ns()
-        total_time_ms = (end_timer - start_timer) / 1000000  # Convert nanoseconds to microseconds
-
-        # Include the time as the 16th float
-        second_part = struct.pack("<8f", *vector[8:], total_time_ms)  # Last 7 floats and the time
-
-        # Transmit the second part
+        total_time_ms = (end_timer - start_timer) / 1000000
+        print(f"Transmission Time: {total_time_ms} ms")
+        second_part = struct.pack("<8f", *vector[8:], total_time_ms)
         result2 = radio.write(second_part)
 
         if not result1 or not result2:
             print("Transmission failed or timed out")
+            GPIO.output(LED2_PIN, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(LED2_PIN, GPIO.LOW)
         else:
-            print(
-                "Full vector transmitted! Time to Transmit:",
-                f"{total_time_ms} ms. Sent: {vector}",
-            )
-
-        time.sleep(1)
+            print("Full vector transmitted! Time to Transmit:", f"{total_time_ms} ms. Sent: {vector}")
+            GPIO.output(LED1_PIN, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(LED1_PIN, GPIO.LOW)
+            
+        time.sleep(0.1)  # Small delay to help with synchronization
 
 if __name__ == "__main__":
     try:
-        transmit_data()
+        main()
     except KeyboardInterrupt:
         print("Keyboard Interrupt detected. Powering down radio.")
         radio.powerDown()
+        GPIO.cleanup()
